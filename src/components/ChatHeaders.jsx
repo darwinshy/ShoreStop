938b72278dccb2a6cd72f44bcf2451a15133f893
Shashwat Priyadarshy <shatish123456789@gmail.com>
2022-07-13 23:44:06 +0530

    Added rest of the components and theme

--- src/components/ChatHeaders.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ChatHeaders = () => {
-  return (
-    <div>ChatHeaders</div>
-  )
-}
-
-export default ChatHeaders
---
+++ src/components/Ecommerce/index.jsx
@@ -0,0 +1,7 @@
+export { default as Activities } from './Activities';
+export { default as Branding } from './Branding';
+export { default as Earnings } from './Earnings';
+export { default as RecentTransactions } from './RecentTransactions';
+export { default as Revenue } from './Revenue';
+export { default as SalesOverview } from './SalesOverview';
+export { default as WeeklyStats } from './WeeklyStats';
